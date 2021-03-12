6c3034eb90d15e96ecb5495f97c97dbfbdb1988c
Bernd Holbein <bernd.holbein@googlemail.com>
2021-03-12 22:12:37 +0100

    Click Events

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,8 +3,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog.';
-  const likes = 50;
 
   return (
     <div className="App">

b8ed544d555eec1b34c400b9a36374fdd95514fd
Bernd Holbein <bernd.holbein@googlemail.com>
2021-03-19 18:41:24 +0100

    json server

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -8,8 +8,6 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
-    const [name, setName] = useState('mario');
-
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
         setBlogs(newBlogs);
@@ -19,13 +17,11 @@ const Home = () => {
     // important don't change state in useEffect()
     useEffect(() => {
         console.log('use effect ran');
-        console.log(name);
-    }, [name]);
+    }, []);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-            <button onClick={() => setName('luigi')}>change name</button>
         </div>
     );
 }

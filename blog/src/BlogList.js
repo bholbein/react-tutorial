2c507b35868ef840c0119314a50c73c6eeb02946
Bernd Holbein <bernd.holbein@googlemail.com>
2021-04-03 15:33:14 +0200

    Reversed blog posts on list

--- blog/src/BlogList.js
+++ blog/src/BlogList.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
 
-const BlogList = ({blogs, title}) => {
-    
-    return ( 
+const BlogList = ({ blogs, title }) => {
+
+    return (
         <div className="blog-list">
             <h2>{title}</h2>
-            {blogs.map((blog) =>(
-               <div className="blog-preview" key={blog.id}>
-                   <Link to={`/blogs/${blog.id}`}>
-                        <h2>{ blog.title }</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
-                   </Link> 
-               </div> 
-            ))}
+                    </Link>
+                </div>
+            )).reverse() 
+            } {/*reverse blog posts chronologically*/}
         </div>
-     );
+    );
 }
- 
+
 export default BlogList;
+
